92b4ade90989fe156c8aefd2ffb7aa31de0be3db
mkkr <mkkr@itu.dk>
2019-06-27 17:21:57 +0200

    initial commit with flutter base

---
+++ whisky_app/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(WhiskyApp());
+
+
+class WhiskyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
